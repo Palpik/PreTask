1bc6c53903e06a3523c42278f7e1b86bbac4e411
Pavel Malashchanka <ozigodde@gmail.com>
2022-01-26 10:18:11 +0300

    Исправлены ошибки и созданы юнит тесты

--- NodFinder/GCD.cs
+++ NodFinder/GCD.cs
@@ -28,15 +28,24 @@ namespace NodFinder
         }
         public static int Euclid(int num1, int num2, int num3)
         {
-            return Euclid(Euclid(num1, num2), num3);
+            if (num1 == 0 && num2 == 0)
+                return Euclid(0, num3);
+            else
+                return Euclid(Euclid(num1, num2), num3);
         }
         public static int Euclid(int num1, int num2, int num3, int num4)
         {
-            return Euclid(Euclid(num1, num2, num3), num4);
+            if (num1 == 0 && num2 == 0 && num3 == 0)
+                return Euclid(0, num4);
+            else
+                return Euclid(Euclid(num1, num2, num3), num4);
         }
         public static int Euclid(int num1, int num2, int num3, int num4, int num5)
         {
-            return Euclid(Euclid(num1, num2, num3, num4), num5);
+            if (num1 == 0 && num2 == 0 && num3 == 0 && num4 == 0)
+                return Euclid(0, num5);
+            else
+                return Euclid(Euclid(num1, num2, num3, num4), num5);
         }
     }
 }
--- NodFinderTests/GCDTests.cs
+++ NodFinderTests/GCDTests.cs
@@ -25,9 +25,46 @@ namespace NodFinder.Tests
         [DataRow(21, -252, -105)]
         [DataRow(1,7,13)]
         [DataRow(1,13,7)]
-        public void EuclidTest(int expectedGCD, int num1, int num2)
+        public void Euclid_TwoNumbers_ReturnsGCD(int expectedGCD, int num1, int num2)
         {
             int realGCD = GCD.Euclid(num1, num2);
+            Assert.AreEqual(realGCD, expectedGCD);
+        }
+
+        [DataTestMethod()]
+        [DataRow(21, 252, 252, 105)]
+        [DataRow(252, 252, 252, 252)]
+        [DataRow(21, -252, 105, 147)]
+        [DataRow(7, 252, 105, 7)]
+        [DataRow(21, 252, 105, 0)]
+        [DataRow(15, 0, 0, 15)]
+        public void Euclid_ThreeNumbers_ReturnsGCD(int expectedGCD, int num1, int num2, int num3)
+        {
+            int realGCD = GCD.Euclid(num1, num2, num3);
+            Assert.AreEqual(realGCD, expectedGCD);
+        }
+
+        [DataTestMethod()]
+        [DataRow(21, 252, 147, 105, 63)]
+        [DataRow(252, 252, 252, 252, 252)]
+        [DataRow(21, -252, 105, 147, -63)]
+        [DataRow(1, 252, 105, 7, 3)]
+        [DataRow(21, 0, 105, 147, 0)]
+        [DataRow(15, 0, 0, 0, 15)]
+        public void Euclid_FourNumbers_ReturnsGCD(int expectedGCD, int num1, int num2, int num3, int num4)
+        {
+            int realGCD = GCD.Euclid(num1, num2, num3,num4);
+            Assert.AreEqual(realGCD, expectedGCD);
+        }
+
+        [DataTestMethod()]
+        [DataRow(21, 252, 147, 105, 63, 42)]
+        [DataRow(252, 252, 252, 252, 252, 252)]
+        [DataRow(1,1,323,12,23,132)]
+        [DataRow(15,0,0,0,0,15)]
+        public void Euclid_FiveNumbers_ReturnsGCD(int expectedGCD, int num1, int num2, int num3, int num4, int num5)
+        {
+            int realGCD = GCD.Euclid(num1, num2, num3, num4, num5);
             Assert.AreEqual(realGCD, expectedGCD);
         }
     }
