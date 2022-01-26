a50c9be47a3e31731f933bbbd748d7a15ca95d81
Pavel Malashchanka <ozigodde@gmail.com>
2022-01-26 09:28:10 +0300

    Созданы метод для вычисления НОД (Euclid) и юнит тесты

---
+++ NodFinder/GCD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodFinder
+{
+    public class GCD
+    {
+        public static int Euclid(int num1,int num2)
+        {
+            num1 = Math.Abs(num1);
+            num2 = Math.Abs(num2);
+            if (num1 == 0 && num2 == 0)
+                throw new Exception("Все числа равны нулю");
+            if (num1 == num2)
+                return num1;
+            while (num1 != 0 && num2 !=0)
+                if (num1 > num2)
+                    num1 = num1 % num2;
+                else if (num1 < num2)
+                    num2 = num2 % num1;
+            if (num1 == 0)
+                return num2;
+            else
+                return num1;
+        }
+    }
+}
---
+++ NodFinderTests/GCDTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NodFinder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodFinder.Tests
+{
+    [TestClass()]
+    public class GCDTests
+    {
+        [DataTestMethod()]
+        [DataRow(21, 252, 105)]
+        [DataRow(21, 105, 252)]
+        [DataRow(21, 147, 105)]
+        [DataRow(21, 42, 105)]
+        [DataRow(21, 42, 63)]
+        [DataRow(21, 42, 21)]
+        [DataRow(21, 21, 21)]
+        [DataRow(21, 0, 21)]
+        [DataRow(21, 21, 0)]
+        [DataRow(21, -252, 105)]
+        [DataRow(21, -252, -105)]
+        [DataRow(1,7,13)]
+        [DataRow(1,13,7)]
+        public void EuclidTest(int expectedGCD, int num1, int num2)
+        {
+            int realGCD = GCD.Euclid(num1, num2);
+            Assert.AreEqual(realGCD, expectedGCD);
+        }
+    }
+}
